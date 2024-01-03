11044c7d6700d393913d2fe5eb550004c330c5ee
CarminaCristina1 <145750733+CarminaCristina1@users.noreply.github.com>
2024-01-03 17:57:08 +0200

    displayingWidgets

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,30 +1,28 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include<dialog.h>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->pushButton->setText("close");
-    connect(ui->horizontalSliderConectat, SIGNAL(valueChanged(int)),
-            ui->progressBar_2, SLOT(setValue(int)));
-
-    connect(ui->horizontalSliderConectat, SIGNAL(valueChanged(int)),
-            ui->progressBar_3, SLOT(setValue(int)));
-
-    disconnect(ui->horizontalSliderConectat, SIGNAL(valueChanged(int)),
-            ui->progressBar_3, SLOT(setValue(int)));
-
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    setCentralWidget(ui->plainTextEdit);
 }
 
 void MainWindow::on_actionNew_Window_triggered()
 {
+    //pt interactiunea cu o singura fereastra cand ambele sunt deschide
+    //Dialog dialog;
+    //dialog.setModal(true);
+    //dialog.exec();
 
+    //pt interactiunea cu ambele ferestre deodata
+    dialog=new Dialog(this);
+    dialog->show();
 }
-
